bdff469ab4ff79f261788267a38e92e3ec44aeda
maroof2424 <maroof96965@gmail.com>
2025-10-14 17:01:53 +0500

    Week12 Day3

--- Month1-6/Month3_ML_DL_APIs/FastAPI_Projects/Week12_FastAPI/app/schemas.py
+++ Month1-6/Month3_ML_DL_APIs/FastAPI_Projects/Week12_FastAPI/app/schemas.py
@@ -1,7 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel ,Field ,validator  
+from typing import Union
 
 class IrisInput(BaseModel):
-    sepal_length : float
-    sepal_width : float
-    petal_length : float
-    petal_width : float
+    sepal_length : Union[float, int] = Field(...,gt = 0, lt = 10 ,description = "Sepal Length in cm")
+    sepal_width : Union[float, int] = Field(...,gt = 0, lt = 10 ,description = "Sepal Width in cm")
+    petal_length : Union[float, int] = Field(...,gt = 0, lt = 10 ,description = "Petal Length in cm")
+    petal_width : Union[float, int] = Field(...,gt = 0, lt = 10 ,description = "Sepal Width in cm")
+
+    @validator("*")
+    def check_float(cls,v):
+        if not isinstance(v,(float,int)):
+            raise ValueError("Value must be numerical")
+        return v
