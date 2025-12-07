19504164aaf3ad31eea24c52443a4cb26e2c76d7
maroof2424 <maroof96965@gmail.com>
2025-12-07 12:07:07 +0500

    test github pages

--- Month1-6/Month5_Web_Cloud/Cloud/Week20_Cloud/frontend/script.js
+++ Month1-6/Month5_Web_Cloud/Cloud/Week20_Cloud/frontend/script.js
@@ -3,7 +3,6 @@ async function predict() {
     const f2 = parseFloat(document.getElementById("f2").value);
     const f3 = parseFloat(document.getElementById("f3").value);
 
-    // Temporary demo logic
     const prediction = (f1 + f2 + f3) > 50 ? "High" : "Low";
 
     document.getElementById("result").innerText = "Prediction: " + prediction;
