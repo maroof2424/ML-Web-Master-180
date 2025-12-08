c2b2614a4ef64eeb1b2577e4a75f6faf5f5c0763
maroof2424 <maroof96965@gmail.com>
2025-12-08 20:56:40 +0500

    Week20 Day3

--- Month1-6/Month5_Web_Cloud/Cloud/Week20_Cloud/app/main.py
+++ Month1-6/Month5_Web_Cloud/Cloud/Week20_Cloud/app/main.py
@@ -1,1 +1,23 @@
-# main.py - starter file
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+
+app = FastAPI()
+
+model = joblib.load("model.pkl")
+
+class InputData(BaseModel):
+    feature1: float
+    feature2: float
+    feature3: float
+
+@app.post("/predict")
+def predict(data: InputData):
+    arr = np.array([[data.feature1, data.feature2, data.feature3]])
+    pred = model.predict(arr)
+    return {"prediction": int(pred[0])}
+
+@app.get("/")
+def home():
+    return {"status": "backend running!"}
